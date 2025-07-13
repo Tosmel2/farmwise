350339d7f185f4fb301e761ef533bc4c16c372ce
Tosmel <adewaletosin0808@gmail.com>
2025-07-13 21:30:57 +0100

    feat: Farmwise backend broiler plate with java springboot set up

---
+++ farmwise-backend/farmwise-backend/src/main/java/com/farmwise/farmwise_backend/FarmwiseApplication.java
@@ -0,0 +1,13 @@
+package com.farmwise.farmwise_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmwiseApplication.class, args);
+	}
+
+}
